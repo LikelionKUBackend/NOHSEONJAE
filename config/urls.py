7693498a93350127f8ab63d5cf8a056153df7b41
SHTJSWO <sunj0414@naver.com>
2023-05-23 20:28:22 +0900

    model

--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from hello import views
+
 urlpatterns = [
-    path("", include("hello.urls")),
+    path("hello/", views.index),
     path('admin/', admin.site.urls),
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
 
-def home(request):
-    return HttpResponse("Hello World!")
+def index(request):
+    response = ""
+    for i in range(1, 4):  # i는 몇 번째 세트인지를 나타냄
+        response += "회원님~! {} 세트 시작하겠습니다\n".format(i)
+        for j in range(1, 11):  # j는 해당 세트에서 동작을 몇 번 반복했는지를 나타냄
+            response += "{}\n".format(j)
+    return HttpResponse(response)
